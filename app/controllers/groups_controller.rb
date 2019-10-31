523a2fd428bc32152379273473c79ad2a4709e49
shuheiyasuda <yasu.canes07@gmail.com>
2019-10-31 12:11:57 +0900

    変更点の改修

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,11 +20,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
