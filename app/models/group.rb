6c105c22e417247e56c7e50c96190c277dd35e8d
shuheiyasuda <yasu.canes07@gmail.com>
2019-11-01 10:51:56 +0900

    メッセージ送信機能の実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,14 @@ class Group < ApplicationRecord
   has_many :users, through: :user_groups
   has_many :user_groups
   has_many :messages
+
+  validates :name, presence:true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.text? ? last_message.text : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
